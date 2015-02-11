2174876ce56bf06d172ff0c54931b93c90016f77
robert <vjroby@gmail.com>
2015-02-11 19:29:54 +0200

    test with database

--- tests/NonceTest.php
+++ tests/NonceTest.php
@@ -1,19 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: robert
- * Date: 25.01.2015
- * Time: 21:05
- */
+
+use Illuminate\Filesystem\Filesystem;
+use Mockery as m;
 
 class NonceTest extends Illuminate\Foundation\Testing\TestCase{
 
-    public function testClass(){
-//        $nonce = new \Vjroby\LaravelNonce\Nonce();
-//
-//        $this->assertTrue($nonce instanceof \Vjroby\LaravelNonce\Nonce);
-        //TODO write tests
-        $this->assertTrue(true);
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testWithDatabase(){
+        $nonce = new \Vjroby\LaravelNonce\Nonce();
+
+        $nonceId = uniqid();
+
+        $nonce->setNonce($nonceId, '');
+
+        $nonce = DB::table('nonce')->where('id', $nonceId)->get();
+
+        $this->assertTrue(count($nonce) == 1);
+
+        $this->assertEquals($nonceId, $nonce[0]->id);
 
     }
 
@@ -37,5 +45,51 @@ class NonceTest extends Illuminate\Foundation\Testing\TestCase{
 //        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
         return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['config']->set('database.default', 'testing');
+
+        $a = __DIR__.'/../src/migrations';
+
+        $this->app['config']->set('database.connections.testing', array(
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ));
+
+        $this->migrate();
+    }
+
+    public function migrate()
+    {
+
+        Schema::dropIfExists('nonce');
+
+
+
+        Schema::create('nonce', function(\Illuminate\Database\Schema\Blueprint $table){
+            $table->string('id',255);
+            $table->string('data',255)->nullable();
+            $table->nullableTimestamps();
+
+            $table->unique(['id', 'data']);
+            $table->index(['id', 'data']);
+        });
+
+//
+//        $fileSystem = new Filesystem;
+////        $classFinder = new ClassFinder;
+//
+//        foreach($fileSystem->files(__DIR__ . "/../src/migrations") as $file)
+//        {
+//            $fileSystem->requireOnce($file);
+//            $migrationClass = new CreateNonceTable();
+//
+//            $migrationClass->up();
+//        }
     }
 }
